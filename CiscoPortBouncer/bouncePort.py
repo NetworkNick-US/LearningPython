498ce5a1e97915a6bdccb08d3ac9ee258fc31f34
NetworkNick <nikwilliams54@gmail.com>
2022-02-01 22:43:30 -0500

    Cisco port-bouncer with Netmiko

---
+++ CiscoPortBouncer/bouncePort.py
@@ -0,0 +1,34 @@
+import netmiko, os, platform, subprocess, time, datetime
+
+class style():
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    BLUEBACKGROUND = '\x1b[1;37;46m'
+
+networkDevice = {
+    'device_type': 'cisco_ios',
+    'host':   '192.168.255.10',
+    'username': 'un',
+    'password': 'pw',
+    'port' : 22,
+    'secret': 'suprise',
+}
+
+network_connection = ConnectHandler(**networkDevice)
+
+print(style.BLUE + "We are going to be shutting down interface {} and waiting 5 seconds before turning it back on".format(suppliedInterface) + style.RESET)
+userConfirm = input(style.RED + "WARNING: IF YOUR UPLINK TO THE NETWORK DEVICE USES THE ABOVE INTERFACE, THIS SCRIPT WILL BREAK YOUR CONNECTION TO THE REMOTE DEVICE.\n Please enter 'YES' to confirm that you want to proceed. [NO]\n" + style.RESET)
+if str(userConfirm).upper() == "YES":
+    #Future use - readability but no delay after shutdown:
+    #config_commands = [suppliedInterface, "shutdown", "no shutdown"]
+    #network_connection.send_config_set(config_commands)
+else:
+    print("Script aborted. No actions have been performed on the remote device")
