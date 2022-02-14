5a53a473be57261c5292be4e3ceea59e608a2910
NetworkNick <nikwilliams54@gmail.com>
2022-02-13 19:42:30 -0500

    Expand Class inheritance and function capabilities

--- Core/core.py
+++ Core/core.py
@@ -15,12 +15,22 @@ class Cisco:
         }
 
         def login(self)
-            return netmiko.ConnectHandler(**self.connection_data)
+            self.conn = netmiko.ConnectHandler(**self.connection_data)
+
+
+class CiscoIOSXE(Cisco):
+
+    def show_run(self):
+        return self.conn.send_command("show run")
+
+    def show_log(self):
+        return self.conn.send_command("show log")
+
 
 UN = input("Enter your username: ")
 PW = getpass.getpass("Enter your password: ", stream=None)
-Core_RTR = Cisco("Core1", "192.168.255.1", "IOS XE", UN, PW)
-Office_Switch = Cisco("SWITCH1", "192.168.255.2", "IOS XE", UN, PW)
+Core_RTR = CiscoIOSXE("Core1", "192.168.255.1", "IOS XE", UN, PW)
 
-Core_RTR_connection = Core_RTR.login()
-Office_Switch = Office_Switch.login()
+Core_RTR.login()
+print(Core_RTR.show_run())
+print(Core_RTR.show_log())
