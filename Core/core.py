20990b3db894f2c18ff5db860860f3687efc87ab
NetworkNick <nikwilliams54@gmail.com>
2022-02-13 20:14:07 -0500

    Fix Syntax issues

--- Core/core.py
+++ Core/core.py
@@ -2,7 +2,7 @@ import netmiko
 import getpass
 
 
-class Cisco:
+class CiscoBase:
 
     def __init__(self, hostname, ip, device_type, username, password):
 
@@ -14,17 +14,16 @@ class Cisco:
             'password': password
         }
 
-        def login(self):
-            self.conn = netmiko.ConnectHandler(**self.connection_data)
+    def login(self):
+        self.connect = netmiko.ConnectHandler(**self.connection_data)
 
-
-class CiscoIOSXE(Cisco):
+class CiscoIOSXE(CiscoBase):
 
     def show_run(self):
-        return self.conn.send_command("show run")
+        return self.connect.send_command("show run")
 
     def show_log(self):
-        return self.conn.send_command("show log")
+        return self.connect.send_command("show log")
 
 
 UN = input("Enter your username: ")
