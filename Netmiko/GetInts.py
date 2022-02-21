7163ee344aeb38d86b4d5421345870085bd84deb
NetworkNick <nikwilliams54@gmail.com>
2022-02-20 21:31:55 -0500

    Fix white space.

--- Netmiko/GetInts.py
+++ Netmiko/GetInts.py
@@ -4,9 +4,7 @@ import LogUtility
 
 class CiscoBase:
 
-    
     def __init__(self, hostname, ip, device_type=None, username=None, password=None):
-        
         self.hostname = hostname
         self.connection_data = {
             'ip': ip,
@@ -16,22 +14,19 @@ class CiscoBase:
         }
 
     def login(self):
-        
         return netmiko.ConnectHandler(**self.connection_data)
 
 
 class CiscoRTR(CiscoBase):
 
-    
     def populate_interface_list(self):
-
         connect = self.login()
         sh_ip_int_brief = connect.send_command("show ip int brief", use_textfsm=True)
         self.interface_list = []
         for interface in sh_ip_int_brief:
             self.interface_list.append(interface['intf'])
 
-            
+
 CoreRTR = CiscoRTR("Core1", "192.168.255.254", "cisco-ios", "localAdmin", "SuperSecure123!!")
 CoreRTR.populate_interface_list()
 ints = CoreRTR.interface_list
