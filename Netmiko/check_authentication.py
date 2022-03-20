8cba05db53c22db30ceeb4e04a5329ce534f1d34
NetworkNick <nikwilliams54@gmail.com>
2022-03-19 20:37:53 -0400

    Catch auth failure and prompt for new credentials

--- Netmiko/check_authentication.py
+++ Netmiko/check_authentication.py
@@ -1,3 +1,4 @@
+import getpass
 import sys
 from netmiko import ConnectHandler
 from netmiko import NetmikoAuthenticationException as AuthenticationFailure
@@ -6,8 +7,9 @@ try:
     connection = ConnectHandler('192.169.255.1', username='cisco', password='supersecure', device_type='cisco_ios')
 except AuthenticationFailure as e:
     output = e
-    sys.exit()
-else:
-    output = connection.send_command('show run')
+    username = input('Enter your username: ')
+    password = getpass.getpass('Enter your password: ')
+    connection = ConnectHandler('192.169.255.1', username=username, password=password, device_type='cisco_ios')
 finally:
     print(output)
+    print(connection.send_command('show run'))
