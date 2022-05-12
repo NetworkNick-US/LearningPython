a6e760a34b88f3db1dac5760527f1cdb1900de92
Network Nick <75176169+NetworkNick-US@users.noreply.github.com>
2022-05-12 14:25:17 -0400

    Update networknick.py

--- Netmiko/networknick.py
+++ Netmiko/networknick.py
@@ -69,3 +69,21 @@ class CiscoRouter(Cisco):
         for interface in sh_ip_int_brief:
             self.interface_list.append(interface['intf'])
         return self.interface_list
+
+
+def is_ipv4_address(ip):
+    """ Return True if ipv4 address,
+    False if not
+    """
+    octets = ip.split('.')
+
+    if len(octets) != 4:
+        return False
+    elif any(not octet.isdigit() for octet in octets):
+        return False
+    elif any(int(octet) < 0 for octet in octets):
+        return False
+    elif any(int(octet) > 255 for octet in octets):
+        return False
+
+    return True
