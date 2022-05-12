b8441e4d433be59f4b3c4bd5c61543b2ef66ae4b
Network Nick <75176169+NetworkNick-US@users.noreply.github.com>
2022-05-12 14:26:33 -0400

    Extend capability

--- Netmiko/networknick.py
+++ Netmiko/networknick.py
@@ -72,7 +72,8 @@ class CiscoRouter(Cisco):
 
 
 def is_ipv4_address(ip):
-    """ Return True if ipv4 address,
+    """ 
+    Return True if ipv4 address,
     False if not
     """
     octets = ip.split('.')
